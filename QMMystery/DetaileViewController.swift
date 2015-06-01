011aecdd839d04d0033c359821453fcf88cc31de
QiMENG <616684889@qq.com>
2015-06-01 16:49:47 +0800

    .

---
+++ QMMystery/DetaileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetaileViewController.swift
+//  QMMystery
+//
+//  Created by QiMENG on 15/6/1.
+//  Copyright (c) 2015年 QiMENG. All rights reserved.
+//
+
+import UIKit
+
+class DetaileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
